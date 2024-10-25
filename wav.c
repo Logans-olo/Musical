428f9e49a8751d95095799d2298a1f9a7a897362
Logan-solo <lsolonch@purdue.edu>
2024-10-24 21:10:34 -0400

    Changed the way the wav file is generated to be able to take a user MIDI as an input then generate A note based off of that frequency.

--- wav.c
+++ wav.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 #include <string.h>
 #include <math.h>
-
+char *fileInput();
+//float round(float);
 typedef struct wav {
 	char riff[4];
 	int32_t flength;
@@ -20,15 +21,24 @@ typedef struct wav {
 	int dlength;
 } wav_header;
 
-void genWav() {
+int main() {
 	const int sample_rate = 8000;
-
+	FILE * fpi = fopen(fileInput(), "r");
+	if (!fpi) { 
+		printf("File was unable to be opened Succesfully \n");
+	}
+	float frequency;
+	fscanf(fpi, "%f", &frequency);
+	printf("%f\n", frequency);
 	wav_header wavh;
 	strncpy(wavh.riff, "RIFF", 4);
 	strncpy(wavh.wave, "WAVE", 4);
 	strncpy(wavh.fmt, "fmt ", 4);  // 'fmt ' should include the space
 	strncpy(wavh.data, "data", 4);
+	frequency = round(440 * pow(2, (frequency - 69) / 12)); // Calculate the music not by frequency from the midi number, see here: 
+	//https://www.fachords.com/note-frequencies/#:~:text=Each%20note%20in%20music%20corresponds%20to%20a%20specific,a%20guitar%20%28A2%29%20typically%20vibrates%20at%20110%20Hz.
 
+	printf("%f\n", frequency);
 	wavh.chunk_size = 16;
 	wavh.format_tag = 1;
 	wavh.chan_num = 1;
@@ -36,17 +46,17 @@ void genWav() {
 	wavh.bits_per_sample = 16;
 	wavh.bytes_per_sample = (wavh.bits_per_sample / 8) * wavh.chan_num;
 	wavh.bytes_per_second = sample_rate * wavh.bytes_per_sample;
-
+	
 	const int duration = 10;
 	const int buffer_size = sample_rate * duration;
 
 	wavh.dlength = buffer_size * wavh.bytes_per_sample;
-	wavh.flength = 36 + wavh.dlength;  // Correct file length
+	wavh.flength = 36 + wavh.dlength;
 
 	short int buffer[buffer_size];
 	memset(buffer, 0, buffer_size * sizeof(short int));
 	for (int i = 0; i < buffer_size; i++) {
-		buffer[i] = (short int)((cos(2 * M_PI * 256 * i / sample_rate)) * 2000);  // Normalized cosine
+		buffer[i] = (short int)((cos(2 * M_PI * frequency * i / sample_rate)) * 2000);  // Generates the note using the frequency number that is passed in
 	}
 
 	FILE *fp = fopen("test.wav", "wb");
@@ -56,3 +66,17 @@ void genWav() {
 	printf("WAV file generated successfully.\n");
 	return 0;
 }
+
+char* fileInput() { 
+	char * file = malloc(50);
+	printf("What is the name of the file to read in from: ");
+	scanf("%49s", file);
+	
+	return file;
+}
+// float round (float num) { 
+// 	num = num * 100;
+// 	num += .5;
+// 	num = (int)num;
+// 	return num / 100;
+// }
